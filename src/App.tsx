ab76068c6f866d0ae9560ea049b34498755a6478
tacowasabii <godseoril812@gmail.com>
2023-01-25 21:10:36 +0900

    shoppinglistform component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { useState } from "react";
+import Item from "./models/item";
 import ShoppingList from "./components/ShoppongList";
+import ShoppingListForm from "./components/ShoppingListForm";
+import { v4 as getId } from "uuid";
 
 function App() {
-  const items = [{ id: "1", product: "bag", quantity: 3 }];
+  const [items, setItems] = useState<Item[]>([]);
+  const addItem = (product: string, quantity: number) => {
+    console.log("MADE TO THE APP COMPONENT!");
+    setItems([...items, { id: getId(), product, quantity }]);
+  };
   return (
     <div>
       <ShoppingList items={items} />
+      <ShoppingListForm onAddItem={addItem} />
     </div>
   );
 }
